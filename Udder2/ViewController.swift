cb8f9d32e2816990d2eab1b12ea43589518de63d
Jordan Booker <jordanlbooker2020@gmail.com>
2019-09-06 19:58:25 -0400

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by jbooker2016 on 9/6/19.
+//  Copyright © 2019 jbooker2016. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
